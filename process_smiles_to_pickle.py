2165cba011941d4f4b614633beb2a3a1045ec11b
Joseph Heenan <jth82@drexel.edu>
2017-10-29 11:54:17 +0100

    Updating smiles script

--- process_smiles_to_pickle.py
+++ process_smiles_to_pickle.py
@@ -10,10 +10,20 @@ def main() :
 
     sdf_root_path = "/media/data/pubchem/SDF"
     i = 0
-    keys = list()
-    values = list()
+
+    all_keys = list()
+    all_values = list()
+
     for path, dirs, filenames in os.walk(sdf_root_path) :
         for filename in filenames:
+
+            new_file_name = os.path.join("/media/data/pubchem/smiles/",filename.replace(".sdf","_smiles.csv"))
+
+            if os.path.exists(new_file_name) :
+                continue
+
+            keys = list()
+            values = list()
 
             start = time.time()
             filepath = os.path.join(sdf_root_path,filename)
@@ -24,18 +34,20 @@ def main() :
                 smiles = mol.GetProp("PUBCHEM_OPENEYE_ISO_SMILES")
                 keys.append(cid)
                 values.append(smiles)
+                all_keys.append(cid)
+                all_values.append(smiles)
             end = time.time()
             print("Processed file number: {0} in {1} seconds".format(i, end - start))
             i = i + 1
 
             df = pd.DataFrame({"PUBCHEM_CID" : keys, "SMILES" : values},index=keys)
-            df.to_csv(os.path.join("/media/data/pubchem/smiles/",filename.replace(".sdf","_smiles.csv")),index=False)
+            df.to_csv(new_file_name,index=False)
 
 
     print("Done mapping CIDs to smiles, storing as pickle")
 
     with open("cid_map.pickle","wb") as f:
-        pickle.dump((keys,values),f,protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump((all_keys,all_values),f,protocol=pickle.HIGHEST_PROTOCOL)
 
     print("Stored data as pickle")
     print("Done")
