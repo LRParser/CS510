a841b952a43e3a4cac73bd065859e38d31206447
Joe <joe@joe.com>
2017-10-29 11:03:44 +0000

    Handle gzipped downloads

--- process_smiles_to_pickle.py
+++ process_smiles_to_pickle.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from rdkit import Chem
 import pickle
 import time
+import gzip
 
 def main() :
     print("Processing PubChem FTP Download")
@@ -17,7 +18,7 @@ def main() :
     for path, dirs, filenames in os.walk(sdf_root_path) :
         for filename in filenames:
 
-            new_file_name = os.path.join("/media/data/pubchem/smiles/",filename.replace(".sdf","_smiles.csv"))
+            new_file_name = os.path.join("/media/data/pubchem/smiles/",filename.replace(".sdf.gz","_smiles.csv"))
 
             if os.path.exists(new_file_name) :
                 continue
@@ -27,21 +28,23 @@ def main() :
 
             start = time.time()
             filepath = os.path.join(sdf_root_path,filename)
-            suppl = Chem.SDMolSupplier(filepath)
-            for mol in suppl:
-                if mol is None: continue
-                cid = mol.GetProp("PUBCHEM_COMPOUND_CID")
-                smiles = mol.GetProp("PUBCHEM_OPENEYE_ISO_SMILES")
-                keys.append(cid)
-                values.append(smiles)
-                all_keys.append(cid)
-                all_values.append(smiles)
-            end = time.time()
-            print("Processed file number: {0} in {1} seconds".format(i, end - start))
-            i = i + 1
+            
+            with gzip.open(filepath,'rb') as myfile:
+                suppl = Chem.ForwardSDMolSupplier(myfile)
+                for mol in suppl:
+                    if mol is None: continue
+                    cid = mol.GetProp("PUBCHEM_COMPOUND_CID")
+                    smiles = mol.GetProp("PUBCHEM_OPENEYE_ISO_SMILES")
+                    keys.append(cid)
+                    values.append(smiles)
+                    all_keys.append(cid)
+                    all_values.append(smiles)
+                end = time.time()
+                print("Processed file number: {0} in {1} seconds".format(i, end - start))
+                i = i + 1
 
-            df = pd.DataFrame({"PUBCHEM_CID" : keys, "SMILES" : values},index=keys)
-            df.to_csv(new_file_name,index=False)
+                df = pd.DataFrame({"PUBCHEM_CID" : keys, "SMILES" : values},index=keys)
+                df.to_csv(new_file_name,index=False)
 
 
     print("Done mapping CIDs to smiles, storing as pickle")
