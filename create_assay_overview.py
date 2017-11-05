a90bb66bf1045b4628d954b5f7916c90b8487a54
Joseph Heenan <jth82@drexel.edu>
2017-11-05 16:46:16 +0100

    Script to create overview of assays

--- create_assay_overview.py
+++ create_assay_overview.py
@@ -6,16 +6,11 @@ import pickle
 import time
 
 def main():
-    print("Loading data from pickle")
+    print("Loading SMILES/CIDs from reference CSV")
 
-    #with open("cid_map.pickle","rb") as f:
-    #    (keys, values) = pickle.load(f)
-
-    pd.read_csv("/media/data/pubchem/summary.csv")
-
-
+    root_df = pd.read_csv("/media/data/pubchem/summary.csv",index_col="PUBCHEM_CID")
     root_path = "/media/data/pubchem/Data"
-    root_df = pd.DataFrame({"PUBCHEM_CID" : keys, "SMILES" : values})
+
     df_list = list()
 
     for path, dirs, filenames in os.walk(root_path) :
@@ -28,24 +23,51 @@ def main():
             for path, dirs, filenames in os.walk(joined_path) :
                 for filename in filenames:
 
+                    if filename not in ["1030.csv","1379.csv"]:
+                        continue
+
+                    assay_name = filename.replace(".csv","")
+                    print("Parsing: {0}".format(assay_name))
+
                     file_path = os.path.join(joined_path,filename)
 
                     # Open each assay result
 
                     df = pd.read_csv(file_path)
 
+                    df = df.dropna(subset=["PUBCHEM_CID"])
+
+                    #for index, row in df['PUBCHEM_CID'].iteritems():
+                    #    if pd.isnull(row):
+                    #        print('index:', index, 'isnull')
+
                     df["ACTIVITY_OUTCOME"] = df["PUBCHEM_ACTIVITY_OUTCOME"] == 'Active'
-                    df["ACTIVITY_OUTCOME"].astype(bool)
+                    df["ACTIVITY_OUTCOME"].astype(int)
 
-                    cid_series = df["PUBCHEM_CID"].astype(int)
-                    outcome_series = df["ACTIVITY_OUTCOME"]
-                    #smiles_series = list()
-                    #for cid in cid_series:
-                    #    smile = values[keys.index(cid)]
-                    #    smiles_series.append(smile)
+                    df["PUBCHEM_CID"] = df["PUBCHEM_CID"].astype(int)
+                    cid_series = df["PUBCHEM_CID"].values
 
-                    assay_name = filename.replace(".csv","")
-                    parsed_df = pd.DataFrame({"PUBCHEM_CID" : cid_series, assay_name : outcome_series, "SMILES" : smiles_series})
+
+                    #cid_series = pd.to_numeric()
+                    outcome_series = df["ACTIVITY_OUTCOME"].values
+                    smiles_series = list()
+                    cids_kept = list()
+                    outcomes_kept = list()
+                    for cid, outcome in zip(cid_series,outcome_series):
+                        print(cid)
+                        try :
+                            ref_row = root_df.iloc[cid]
+                            if(ref_row is not None and ref_row["SMILES"] is not None) :
+                                smile = ref_row.SMILES
+                                cids_kept.append(cid)
+                                smiles_series.append(smile)
+                                outcomes_kept.append(outcome)
+
+                        except :
+                            print("Passed on CID")
+
+                    parsed_df = pd.DataFrame({"PUBCHEM_CID" : cids_kept, assay_name : outcomes_kept, "SMILES" : smiles_series})
+                    parsed_df.set_index("PUBCHEM_CID")
 
                     df_list.append(parsed_df)
                     print(assay_name)
@@ -59,7 +81,7 @@ def main():
 
     df_full = pd.concat(df_list)
 
-    root_df.to_csv("/media/data/pubchem/pcba_full.csv")
+    df_full.to_csv("/media/data/pubchem/assay_summary.csv",index=False)
 
 if __name__ == "__main__" :
     main()
