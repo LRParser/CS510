71e6941f18512f9b08090f5e7ed883a15ec02501
Joseph Heenan <jth82@drexel.edu>
2017-12-03 11:07:18 +0100

    Mapping smiles in kekule form and saving as sorted lists

--- create_smiles_mapping.py
+++ create_smiles_mapping.py
@@ -16,108 +16,69 @@ def main() :
     except :
         print("Results directory already exists")
 
-    i = 0
+    compound_read_count = 0
     max_smiles_len = 50
-
-    processed_files = os.listdir(results_path)
-    #processed_files.append("Compound_102125001_102150000_smiles.csv")
-
-
     keys = list()
     values = list()
-    write_dict = False
     overall_start = time.time()
 
-    with open("/media/data/pubchem/summary.csv","w") as f:
+    all_paths = list()
 
-        f.write("PUBCHEM_CID"+","+"SMILES"+os.linesep)
+    for path, dirs, filenames in os.walk(sdf_root_path) :
+        for filename in filenames:
 
-        for path, dirs, filenames in os.walk(sdf_root_path) :
-            for filename in filenames:
+            if "102125001_102150000" in filename:
+                continue
 
-                if "102125001_102150000" in filename:
+            file_path = os.path.join(sdf_root_path, filename)
+            all_paths.append(file_path)
+
+    all_paths.sort()
+
+    for filepath in all_paths:
+
+        print("Processing: {0}".format(filepath))
+        start = time.time()
+
+        with gzip.open(filepath,'rb') as myfile:
+            suppl = Chem.ForwardSDMolSupplier(myfile)
+            for mol in suppl:
+                if mol is None: continue
+                cid = mol.GetProp("PUBCHEM_COMPOUND_CID")
+                try :
+                    Chem.Kekulize(mol)
+                    smiles = Chem.MolToSmiles(mol,kekuleSmiles=True)
+                    if len(smiles) > max_smiles_len:
+                        compound_read_count =compound_read_count + 1
+                        #print("Skipped compound: {0} due to large size".format(cid))
+                        continue
+
+                    keys.append(int(cid))
+                    values.append(smiles)
+                except Exception:
+                    #print(e)
                     continue
+            end = time.time()
 
-                print("Processing: {0}".format(filename))
-
-                #expected_file_name = filename.replace(".sdf.gz", "_smiles.csv")
-                #new_file_name = os.path.join(results_path,expected_file_name)
-
-
-                #if expected_file_name in processed_files:
-                #    print("Skipping: {0}".format(new_file_name))
-                #    i = i + 1
-                #    continue
-
-                #keys = list()
-                #values = list()
-
-                start = time.time()
-                filepath = os.path.join(sdf_root_path,filename)
-
-                with gzip.open(filepath,'rb') as myfile:
-                    suppl = Chem.ForwardSDMolSupplier(myfile)
-                    for mol in suppl:
-                        if mol is None: continue
-                        cid = mol.GetProp("PUBCHEM_COMPOUND_CID")
-                        try :
-                            Chem.Kekulize(mol)
-                            smiles = Chem.MolToSmiles(mol,kekuleSmiles=True)
-                            if len(smiles) > max_smiles_len:
-                                i = i + 1
-                                #print("Skipped compound: {0} due to large size".format(cid))
-                                continue
+        print("Processed file, processed thru compound number: {0} in {1} seconds".format(compound_read_count, end - start))
+        compound_read_count = compound_read_count + 1
 
 
-                            f.write("CID"+cid+","+smiles+os.linesep)
-                            keys.append(int(cid))
-                            values.append(smiles)
-                        except Exception:
-                            #print(e)
-                            continue
-                    end = time.time()
+    overall_end = time.time()
+    secs_elapsed = overall_end - overall_start
+    print("Parsed all smiles in: {0} seconds, or {1} minutes, or {2} hours".format(secs_elapsed,secs_elapsed/60,secs_elapsed/3600))
 
-                    print("Processed file, processed thru compound number: {0} in {1} seconds".format(i, end - start))
-                    i = i + 1
+    #print("Sorting lists and saving pickle")
+    #overall_start = time.time()
+    #keys, values = (list(t) for t in zip(*sorted(zip(keys, values))))
 
-                    #df = pd.DataFrame({"PUBCHEM_CID" : keys, "SMILES" : values},index=keys)
-                    #df.to_csv(new_file_name,index=False)
-
-            f.flush()
-
-        print("Wrote CID info to CSV")
-
-        overall_end = time.time()
-        secs_elapsed = overall_end - overall_start
-        print("Parsed all smiles in: {0} seconds, or {1} minutes, or {2} hours".format(secs_elapsed,secs_elapsed/60,secs_elapsed/3600))
-
-        # Now parse all results smile files into one big file
-        #df_list = list()
-        #processed_files = os.listdir(results_path)
-        #for filename in processed_files :
-        #    print("Processing: {} for summary CSV".format(filename))
-        #    df = pd.read_csv(os.path.join(results_path,filename))
-        #    df_list.append(df)
-
-        #df_full = pd.concat(df_list)
-
-
-        #print("Writing out summary CSV")
-        #df_full.to_csv("/media/data/pubchem/summary.csv",index=False)
-
-        #print("Stored data as CSV")
-
-    if write_dict:
-        print("Saved file, now saving pickle")
-
-        mydict = sorted(dict(zip(keys,values)))
-        with open("/media/data/pubchem/kekulesmiles.pickle","wb") as f:
-            pickle.dump(mydict,f)
-
-    else :
-        with open("/media/data/pubchem/kekulesmiles_tuple.pickle","wb") as f:
-            pickle.dump((keys,values),f)
+    with open("/media/data/pubchem/kekulesmiles_tuple.pickle","wb") as f:
+        pickle.dump((keys,values),f)
     print("Done")
+    overall_end = time.time()
+    secs_elapsed = overall_end - overall_start
+    print("Sorted and saved smiles in: {0} seconds, or {1} minutes, or {2} hours".format(secs_elapsed, secs_elapsed / 60,
+                                                                                   secs_elapsed / 3600))
 
 if __name__ == '__main__':
     main()
